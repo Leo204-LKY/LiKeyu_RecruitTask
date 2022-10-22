31c878d201330529e6f67f875d1c8af32c016606
Leo204_LKY <2498673113@qq.com>
2022-10-23 01:08:40 +0800

    Finishes Task 1

---
+++ Assessment_Tasks/Task_1/main.c
@@ -0,0 +1,40 @@
+#include <REGX52.H>
+#include <INTRINS.H>
+
+void Delay(unsigned int xms)		//@12.000MHz, Delay [xms] ms，定时器溢出中断
+{
+	unsigned char i, j;
+  while(xms)
+  {
+	  i = 2;
+	  j = 239;
+	  do
+	  {
+	  	while (--j);
+	  } while (--i);
+    xms--;
+  }
+}
+
+void main()
+{
+	EA = 1;  // 允许中断
+	EX0 = 1; // 允许外部中断
+	IT0 = 1; // 下降沿触发，P3_2 引脚电平下降时(即按下 K3 按键过程中)触发中断
+	while(1)
+	{
+		P2_0 = 1;
+	}
+}
+
+void Flash() interrupt 0 // 中断服务程序，中断号 0
+{
+	int i;
+	for(i=0;i<3;i++)
+	{
+		P2_0 = 1;
+		Delay(500);
+		P2_0 = 0;
+		Delay(500);
+	}
+}
